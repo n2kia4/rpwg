e61850611665c2040df5de74df7f4527b93957d9
Natsuki Tanaka <n2kia4@gmail.com>
2017-01-08 12:33:55 +0900

    Refactor argument parsing
    Add parse_args function

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,67 @@ const UPPERCASE: &'static [u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const LOWERCASE: &'static [u8] = b"abcdefghijklmnopqrstuvwxyz";
 const SYMBOL: &'static [u8] = b"!\"#$%&'()-=^~\\|@`[]{};:+*,./_<>?";
 
+struct Args {
+    length: usize,
+    quantity: usize,
+    number: bool,
+    upper: bool,
+    symbol: bool,
+}
+
 fn main() {
+    let args = parse_args();
+    let mut vec: Vec<u8> = Vec::new();
+    let mut n = 1;
+
+    vec.write(LOWERCASE).unwrap();
+    if args.number == false {
+        vec.write(NUMBER).unwrap();
+        n += 1;
+    }
+    if args.upper == false {
+        vec.write(UPPERCASE).unwrap();
+        n += 1;
+    }
+    if args.symbol == true {
+        vec.write(SYMBOL).unwrap();
+        n += 1;
+    }
+
+    let mut rng = rand::thread_rng();
+    if n <= args.length {
+        for _ in 0.. args.quantity {
+            let mut mtl: Vec<char> = Vec::new();
+
+            mtl.extend_from_slice(&each_choose(&LOWERCASE));
+
+            if args.number == false {
+                mtl.extend_from_slice(&each_choose(&NUMBER));
+            }
+
+            if args.upper == false {
+                mtl.extend_from_slice(&each_choose(&UPPERCASE));
+            }
+
+            if args.symbol == true {
+                mtl.extend_from_slice(&each_choose(&SYMBOL));
+            }
+
+            let mtl_rand: Vec<char> = (0..args.length - n)
+                .map(|_| *rng.choose(&vec).unwrap() as char)
+                .collect();
+            mtl.extend_from_slice(&mtl_rand);
+
+            rng.shuffle(&mut mtl);
+            let pw: String = mtl.into_iter().collect();
+            println!("{}", pw);
+        }
+    } else {
+        println!("Please specify length more than {}.", n);
+    }
+}
+
+fn parse_args() -> Args {
     let matches = App::new("rpwg")
         .version(crate_version!())
         .author(crate_authors!())
@@ -23,55 +83,12 @@ fn main() {
         .arg(Arg::from_usage("-S 'Include special symbols'"))
         .get_matches();
 
-    let length = value_t!(matches, "length", usize).unwrap();
-    let qty = value_t!(matches, "quantity", usize).unwrap();
-
-    let mut vec: Vec<u8> = Vec::new();
-    let mut n = 1;
-    vec.write(LOWERCASE).unwrap();
-    if ! matches.is_present("0") {
-        vec.write(NUMBER).unwrap();
-        n += 1;
-    }
-    if ! matches.is_present("A") {
-        vec.write(UPPERCASE).unwrap();
-        n += 1;
-    }
-    if matches.is_present("S") {
-        vec.write(SYMBOL).unwrap();
-        n += 1;
-    }
-
-    let mut rng = rand::thread_rng();
-    if n <= length {
-        for _ in 0..qty {
-            let mut mtl: Vec<char> = Vec::new();
-
-            mtl.extend_from_slice(&each_choose(&LOWERCASE));
-
-            if ! matches.is_present("0") {
-                mtl.extend_from_slice(&each_choose(&NUMBER));
-            }
-
-            if ! matches.is_present("A") {
-                mtl.extend_from_slice(&each_choose(&UPPERCASE));
-            }
-
-            if matches.is_present("S") {
-                mtl.extend_from_slice(&each_choose(&SYMBOL));
-            }
-
-            let mtl_rand: Vec<char> = (0..length - n)
-                .map(|_| *rng.choose(&vec).unwrap() as char)
-                .collect();
-            mtl.extend_from_slice(&mtl_rand);
-
-            rng.shuffle(&mut mtl);
-            let pw: String = mtl.into_iter().collect();
-            println!("{}", pw);
-        }
-    } else {
-        println!("Please specify length more than {}.", n);
+    Args {
+        length: value_t!(matches, "length", usize).unwrap(),
+        quantity: value_t!(matches, "quantity", usize).unwrap(),
+        number: matches.is_present("0"),
+        upper: matches.is_present("A"),
+        symbol: matches.is_present("S"),
     }
 }
 
